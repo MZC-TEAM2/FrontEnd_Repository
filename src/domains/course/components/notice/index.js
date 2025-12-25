b03c604419a1f3d751ce63ba2d65390e793972cc
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-25 13:03:55 +0900

    Update index.js

--- src/domains/course/components/notice/index.js
+++ src/domains/course/components/notice/index.js
@@ -1,4 +1,4 @@
-export { default as CourseNoticeBoard } from './CourseNoticeBoard';
-export { default as NoticeList } from './NoticeList';
-export { default as NoticeDetail } from './NoticeDetail';
-export { default as NoticeForm } from './NoticeForm';
+export {default as CourseNoticeBoard} from './CourseNoticeBoard';
+export {default as NoticeList} from './NoticeList';
+export {default as NoticeDetail} from './NoticeDetail';
+export {default as NoticeForm} from './NoticeForm';
