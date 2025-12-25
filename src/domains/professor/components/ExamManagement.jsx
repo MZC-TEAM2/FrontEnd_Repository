6298cdf4cb0a30da3ce40efa8d5c00b61f34cf05
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-25 13:03:56 +0900

    Update ExamManagement.jsx

--- src/domains/professor/components/ExamManagement.jsx
+++ src/domains/professor/components/ExamManagement.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import AssessmentManagement from './AssessmentManagement';
 
-export default function ExamManagement({ courseId }) {
-  return <AssessmentManagement courseId={courseId} mode="EXAM" />;
+export default function ExamManagement({courseId}) {
+    return <AssessmentManagement courseId={courseId} mode="EXAM"/>;
 }
 
 
