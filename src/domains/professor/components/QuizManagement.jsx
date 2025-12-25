4cee6b99f5921c6bafb1bd70dc6e1bdc1279c185
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-25 13:03:56 +0900

    Update QuizManagement.jsx

--- src/domains/professor/components/QuizManagement.jsx
+++ src/domains/professor/components/QuizManagement.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import AssessmentManagement from './AssessmentManagement';
 
-export default function QuizManagement({ courseId }) {
-  return <AssessmentManagement courseId={courseId} mode="QUIZ" />;
+export default function QuizManagement({courseId}) {
+    return <AssessmentManagement courseId={courseId} mode="QUIZ"/>;
 }
 
 
