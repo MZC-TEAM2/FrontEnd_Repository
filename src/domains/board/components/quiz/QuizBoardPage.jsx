7ee24ab1e02ffbb82e79800b7b2cc5db0e8ebb9a
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-25 13:03:54 +0900

    Update QuizBoardPage.jsx

--- src/domains/board/components/quiz/QuizBoardPage.jsx
+++ src/domains/board/components/quiz/QuizBoardPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import BoardListPage from '../common/BoardListPage';
-import { Quiz as QuizIcon } from '@mui/icons-material';
+import {Quiz as QuizIcon} from '@mui/icons-material';
 
 /**
  * 퀴즈 게시판 페이지
@@ -8,18 +8,18 @@ import { Quiz as QuizIcon } from '@mui/icons-material';
  * - 퀴즈 정보 공유
  */
 const QuizBoardPage = () => {
-  return (
-    <BoardListPage
-      boardType="QUIZ"
-      title="퀴즈"
-      icon={<QuizIcon sx={{ fontSize: 32, color: 'primary.main' }} />}
-      basePath="/boards/quiz"
-      createPath="/boards/quiz/create"
-      createButtonText="퀴즈 등록"
-      showViewCount={false}
-      showLikeCount={false}
-    />
-  );
+    return (
+        <BoardListPage
+            boardType="QUIZ"
+            title="퀴즈"
+            icon={<QuizIcon sx={{fontSize: 32, color: 'primary.main'}}/>}
+            basePath="/boards/quiz"
+            createPath="/boards/quiz/create"
+            createButtonText="퀴즈 등록"
+            showViewCount={false}
+            showLikeCount={false}
+        />
+    );
 };
 
 export default QuizBoardPage;
