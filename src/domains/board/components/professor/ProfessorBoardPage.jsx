14ebf224db45e606e91dfd3df4fa711a3052132d
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-25 13:03:54 +0900

    Update ProfessorBoardPage.jsx

--- src/domains/board/components/professor/ProfessorBoardPage.jsx
+++ src/domains/board/components/professor/ProfessorBoardPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import BoardListPage from '../common/BoardListPage';
-import { School as SchoolIcon } from '@mui/icons-material';
+import {School as SchoolIcon} from '@mui/icons-material';
 
 /**
  * 교수 게시판 페이지
@@ -8,17 +8,17 @@ import { School as SchoolIcon } from '@mui/icons-material';
  * - 교수 전용 공지, 자료 공유 등
  */
 const ProfessorBoardPage = () => {
-  return (
-    <BoardListPage
-      boardType="PROFESSOR"
-      title="교수 게시판"
-      icon={<SchoolIcon sx={{ fontSize: 32, color: 'primary.main' }} />}
-      basePath="/boards/professor"
-      createPath="/boards/professor/create"
-      createButtonText="교수 글 작성"
-      showLikeCount={false}
-    />
-  );
+    return (
+        <BoardListPage
+            boardType="PROFESSOR"
+            title="교수 게시판"
+            icon={<SchoolIcon sx={{fontSize: 32, color: 'primary.main'}}/>}
+            basePath="/boards/professor"
+            createPath="/boards/professor/create"
+            createButtonText="교수 글 작성"
+            showLikeCount={false}
+        />
+    );
 };
 
 export default ProfessorBoardPage;
