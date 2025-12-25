3c352c3a7422e1e304fd26a29b39e2d02e315483
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-25 13:03:54 +0900

    Update StudentBoardPage.jsx

--- src/domains/board/components/student/StudentBoardPage.jsx
+++ src/domains/board/components/student/StudentBoardPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import BoardListPage from '../common/BoardListPage';
-import { Groups as GroupsIcon } from '@mui/icons-material';
-import { STUDENT_HASHTAGS } from '../../constants/boardHashtags';
+import {Groups as GroupsIcon} from '@mui/icons-material';
+import {STUDENT_HASHTAGS} from '../../constants/boardHashtags';
 
 /**
  * 학생 게시판 페이지
@@ -9,18 +9,18 @@ import { STUDENT_HASHTAGS } from '../../constants/boardHashtags';
  * - 해시태그 기반 주제별 필터링
  */
 const StudentBoardPage = () => {
-  return (
-    <BoardListPage
-      boardType="STUDENT"
-      title="학생 게시판"
-      icon={<GroupsIcon sx={{ fontSize: 32, color: 'primary.main' }} />}
-      basePath="/boards/student"
-      createPath="/boards/student/create"
-      createButtonText="학생 글 작성"
-      hashtags={STUDENT_HASHTAGS}
-      showHashtagsInTable={true}
-    />
-  );
+    return (
+        <BoardListPage
+            boardType="STUDENT"
+            title="학생 게시판"
+            icon={<GroupsIcon sx={{fontSize: 32, color: 'primary.main'}}/>}
+            basePath="/boards/student"
+            createPath="/boards/student/create"
+            createButtonText="학생 글 작성"
+            hashtags={STUDENT_HASHTAGS}
+            showHashtagsInTable={true}
+        />
+    );
 };
 
 export default StudentBoardPage;
