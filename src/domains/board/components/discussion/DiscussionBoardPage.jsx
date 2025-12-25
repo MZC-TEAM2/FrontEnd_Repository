aa0d721403ece1efd208d21a4a0d2a8568e9fb28
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-25 13:03:54 +0900

    Update DiscussionBoardPage.jsx

--- src/domains/board/components/discussion/DiscussionBoardPage.jsx
+++ src/domains/board/components/discussion/DiscussionBoardPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import BoardListPage from '../common/BoardListPage';
-import { RecordVoiceOver as RecordVoiceOverIcon } from '@mui/icons-material';
-import { DISCUSSION_HASHTAGS } from '../../constants/boardHashtags';
+import {RecordVoiceOver as RecordVoiceOverIcon} from '@mui/icons-material';
+import {DISCUSSION_HASHTAGS} from '../../constants/boardHashtags';
 
 /**
  * 토론 게시판 페이지
@@ -10,18 +10,18 @@ import { DISCUSSION_HASHTAGS } from '../../constants/boardHashtags';
  */
 const DiscussionBoardPage = () => {
 
-  return (
-    <BoardListPage
-      boardType="DISCUSSION"
-      title="토론 게시판"
-      icon={<RecordVoiceOverIcon sx={{ fontSize: 32, color: 'primary.main' }} />}
-      basePath="/discussions"
-      createPath="/discussions/create"
-      createButtonText="토론 시작"
-      hashtags={DISCUSSION_HASHTAGS}
-      showHashtagsInTable={true}
-    />
-  );
+    return (
+        <BoardListPage
+            boardType="DISCUSSION"
+            title="토론 게시판"
+            icon={<RecordVoiceOverIcon sx={{fontSize: 32, color: 'primary.main'}}/>}
+            basePath="/discussions"
+            createPath="/discussions/create"
+            createButtonText="토론 시작"
+            hashtags={DISCUSSION_HASHTAGS}
+            showHashtagsInTable={true}
+        />
+    );
 };
 
 export default DiscussionBoardPage;
